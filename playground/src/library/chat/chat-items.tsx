e13c21c6be0fc36e4061818934991426ccc1723d
wxy1203 <e0817822@u.nus.edu>
2024-04-03 12:51:13 +0800

    Update chat-items.tsx

--- playground/src/library/chat/chat-items.tsx
+++ playground/src/library/chat/chat-items.tsx
@@ -205,23 +205,23 @@ export function AgentJSONBlock (props: ChatItemProps) {
                 </code></pre>
             </View>
 
-            <View>
-                {/* <Card paddingLeft={10} className="codeBoxHeader">
+        </View>
+    );
+}
+
+export function DrawGraphBlock (props: ChatItemProps) {
+    return (
+        <View textAlign='left' paddingLeft={20} paddingRight={20}>
+            <View lineHeight={2}>
+                <Card paddingLeft={10} className="codeBoxHeader">
                     <Heading>
                         Graph
                     </Heading>
-                </Card> */}
-                {/* 下面是用代码画的图 */}
                 
-                {/* 用Json画图 */}
-                {/* <JsonDraw /> */}
-
-                {/* 用HTML画图 */}
-                {/* <div style={{ position: 'relative', height: 0, paddingBottom: '56.25%' }}>
-                    <iframe src="/bar_chart.html" title="Result" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} />
-                </div>      */}
+                {/* 下面是用代码画的图 */}
+                <JsonDraw />
+                </Card>
             </View>
-
         </View>
     );
 }
