f50fe8d03e5635c23280193db465fbd47e39504a
wxy1203 <e0817822@u.nus.edu>
2024-04-09 15:42:46 +0800

    Update chat-items.tsx

--- playground/src/library/chat/chat-items.tsx
+++ playground/src/library/chat/chat-items.tsx
@@ -180,7 +180,7 @@ export function AgentJSONBlock (props: ChatItemProps) {
                     </Heading> */}
                 </Card>
                 
-                {/* Code */}
+                {/* Code Itself */}
                 <pre><code>
                     {props.text}
                 </code></pre>
